f592a941b1216da46636113df01f499692f3d073
Hellia <hellia@kelaun.be>
2021-09-30 11:42:31 +0000

    Fixing network name

--- spectrwm/baraction.sh
+++ spectrwm/baraction.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 while :; do
-				NETSTAT="$(nmcli -p | grep wlan0 | head -1 | awk '{ print $2 }')"
-				NETNAME="$(if [ $NETSTAT == "connected" ]; then echo "$(nmcli -p | grep wlan0 | head -1 | awk '{ print $4 }')"; else echo $NETSTAT; fi)"
+				NETSTAT="$(nmcli -p | grep wlp1s0 | head -1 | awk '{ print $2 }')"
+				NETNAME="$(if [ $NETSTAT == "connected" ]; then echo "$(nmcli -p | grep wlp1s0 | head -1 | awk '{ print $4 }')"; else echo $NETSTAT; fi)"
 
 				VOLUME="$(pactl list sinks | grep '^[[:space:]]Volume:' | head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')%"
 				
